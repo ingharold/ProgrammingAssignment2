860de72dd7d201dd98a394f0ce6bf65ef38dff08
ingharold <71151393+ingharold@users.noreply.github.com>
2020-09-26 14:09:17 -0500

    Revert "Update cachematrix.R"
    This reverts commit 7b9efd215ec15cf23456f7c97e4c61a4fc118286.

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,15 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Estableciendo la matriz
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
-  set <- function(y) {
-    x <<- y
-    i <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(solveinv) i <<- solveinv
-  getinverse <- function() i
-  list(set = set,
-       get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+
 }
 
 
-##Calculando la matriz Inversa
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  i <- x$getinverse()
-  if (!is.null(i)) {
-    message("obteniendo datos de cache")
-    return(i)
-  }
-  data <- x$get()
-  i <- solve(data, ...)
-  x$setinverse(i)
-  i
+        ## Return a matrix that is the inverse of 'x'
 }
